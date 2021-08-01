cac4a34b63f6555a102067f19c3ab52f5b5c0475
javyer12 <frac.guillen47@gmail.com>
2021-07-31 20:51:03 -0600

    style defined

---
+++ dashboard/src/_mocks_/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Francisco Murillo',
+  email: 'javyermurillo1@gmail.com',
+  photoURL: 'https://i.blogs.es/66b2a4/photo-1511367461989-f85a21fda167/1366_2000.jpeg'
+};
+
+export default account;
